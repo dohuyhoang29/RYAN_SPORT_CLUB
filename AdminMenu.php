79b555c8c362a2272ab3407993eb2d6cb45d8922
dohuyhoang29 <hoangdohuy29092002@gmail.com>
2021-04-22 09:05:45 +0700

    ajdfa

--- AdminMenu.php
+++ AdminMenu.php
@@ -1,6 +1,6 @@
 <?php
     require_once('Admin/DatabaseRYAN.php');
-    require_once('initialize.php');//khang nghien vaof beo
+    require_once('initialize.php');//khang nghien vaof beo gay
 ?>
 
 <!DOCTYPE html>
