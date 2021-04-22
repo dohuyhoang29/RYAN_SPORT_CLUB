2b387c24c08834e33b124cae2efad68a2325d32e
dohuyhoang29 <hoangdohuy29092002@gmail.com>
2021-04-22 09:01:52 +0700

    them khang nghien

--- AdminMenu.php
+++ AdminMenu.php
@@ -1,6 +1,6 @@
 <?php
     require_once('Admin/DatabaseRYAN.php');
-    require_once('initialize.php');//ajfdhsasdfasdfasdfsadfasfadfsafsfsaf
+    require_once('initialize.php');//khang nghien
 ?>
 
 <!DOCTYPE html>
