5e9bc1286ecad91f6466abad55515629eac62922
anhtu2291997 <anhtu22919997@gmail.com>
2021-04-22 09:44:06 +0700

    sdfghjk

--- sharesession.php
+++ sharesession.php
@@ -1,4 +1,5 @@
 <p>User: <?php echo isset($_SESSION['username'])?$_SESSION['username']: ''; ?>
     &emsp;
-    <a href="../Admin/LogoutRYAN.php">Logout</a>
+    <a href="../Admin/LogoutRYAN.php">Logout</a> 
+<!-- sdadasfascsaagv 22222222222222222222222222222edc-->
 </p>
